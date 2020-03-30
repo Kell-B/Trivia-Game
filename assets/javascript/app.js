7b1ee834248aa38aeeb125408efb75f0633f5627
Kell Banks <mckellbanks@gmail.com>
2020-03-30 15:32:56 -0600

    added in more pseudo-code to my js file and created my question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,10 +8,119 @@
 
 $(document).ready(function() {
 	// Global Variables and Objects
+	var question1 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer4'
+	};
+
+	var question2 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer1'
+	};
+
+	var question3 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer2'
+	};
+
+	var question4 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer4'
+	};
+
+	var question5 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer1'
+	};
+
+	var question6 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer3'
+	};
+
+	var question7 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer4'
+	};
+
+	var question8 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer1'
+	};
+
+	var question9 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer2'
+	};
+
+	var question10 = {
+		quest   : '',
+		answer1 : '',
+		answer2 : '',
+		answer3 : '',
+		answer4 : '',
+		correct : 'answer4'
+	};
 	// Start Game Functions
-	// Timer/Clock running and display Functions
-	// Right/Wrong answer/ out of time Functions
+	// Create an on click to start the game
+	// Set up start stage
+	// Remove any text/html from previous game
+
+	// Timer/Clock running and timer display Functions
+	// Start Timer
+	// Stop Timer
+	// Timer Counter
+	// Display Timer
+
+	// Right/Wrong answers and out of time display Functions
+	// If answer is right
+	// If answer is wrong
+	// If player runs out of time
+	// Check if right answer was clicked
+	// Moves to next question
+	// When there are no more questions
+
 	// Display current Question Function
+	// Update the stage with question and answer choices
+
 	// Update end stage screen with right wrong and unanswered questions Function
+
 	// Restart the game Function
+	// Create an on click that gets displayed when game is over
 });
