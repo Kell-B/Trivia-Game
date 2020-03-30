88eedc8cdc4b64929bb54e3b8abd1ff7755be1cc
Kell Banks <mckellbanks@gmail.com>
2020-03-30 14:57:03 -0600

    created basic js game structure and psedo-coded functions needed

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Game structure pseudo-code
+// 1. Show a trivia question until player answers it or timer runs out.
+// 2. When player clicks correct answer, show a popup saying so. After 3 seconds move on to the next question (do this without userinput).
+// 3. If the player clicks the wrong answer, let them know and move on to the next question. Simalr to step 2.
+// 4. If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+// 5. If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+// 6. On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+$(document).ready(function() {
+	// Global Variables and Objects
+	// Start Game Functions
+	// Timer/Clock running and display Functions
+	// Right/Wrong answer/ out of time Functions
+	// Display current Question Function
+	// Update end stage screen with right wrong and unanswered questions Function
+	// Restart the game Function
+});
